aa659a183e8371477969df116f908e2ef786278f
vernetto <publicpierre@gmail.com>
2018-12-12 21:27:52 +0100

    all

--- src/test/java/org/pierre/nexusclean/NexuscleanApplicationTests.java
+++ src/test/java/org/pierre/nexusclean/NexuscleanApplicationTests.java
@@ -1,16 +1,28 @@
 package org.pierre.nexusclean;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class NexuscleanApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testUnique() {
+		ArtifactRepository artifactRepository = new ArtifactRepository();
+		artifactRepository.add(new Artifact("artifactId1", "groupId1", "version1", "20181209"));
+		artifactRepository.add(new Artifact("artifactId1", "groupId1", "version2", "20181208"));
+		artifactRepository.add(new Artifact("artifactId1", "groupId2", "version2", "20181208"));
+		List<Artifact> unique = artifactRepository.findArtifactsWithUniqueGA();
+		artifactRepository.printAllArtifacts(unique, System.out);
 	}
 
 }
